b39137a7f534261649f5ada707ea8e09abf9f69f
B13ckp <74647375+B13ckp@users.noreply.github.com>
2020-11-18 11:56:42 -0700

    Create Califiaciones-Imprimir.c

---
+++ Califiaciones-Imprimir.c
@@ -0,0 +1,23 @@
+*Jhovanny Daniel Beltran Lugo 303 	*/
+#include <stdio.h>
+
+int main() {
+   char letra;
+  printf("ingrese una letra de la A a la F: ");
+  scanf("%c",&letra);
+
+  if (letra == 'A'){
+    	printf("10");
+  	}else if (letra == 'B'){
+    	printf("9");
+ 	 }else if (letra == 'C'){
+    	printf("8");
+	}else if (letra == 'D') {
+    	printf("7");
+	}else if (letra == 'E'){
+  		printf("6");
+	}else if (letra == 'F'){
+  		printf("5");
+}
+	return 0;
+}
